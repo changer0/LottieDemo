9a0cef4ef421eea0d3cad70818d3b0844e7266c7
p_zlulzhang <zhanglulu@yuewen.com>
2019-12-25 19:22:35 +0800

    平移动画

--- app/src/main/java/com/lulu/lottiedemo/MainActivity.kt
+++ app/src/main/java/com/lulu/lottiedemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.lulu.lottiedemo
 
+import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,20 +12,22 @@ import kotlinx.android.synthetic.main.activity_main.view.*
 class MainActivity : AppCompatActivity() {
 
     private val tag = "MainActivity"
+    var count = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        lottie_anim.imageAssetsFolder = "images/"
-        lottie_anim.setAnimation("data.json")
-//        LottieCompositionFactory.fromAsset(this, "data.json")
+
+        lottie_anim.playAnimation()
+//        LottieCompositionFactory.fromUrl(this, "https://ptcoopresource-1252317822.file.myqcloud.com/cooperate/operationsmanagement/43a2dfd19c982be1929c8167e778cf6fcad67c1869c2f67d452c2b90536a1b632019121011.zip")
 //            .addListener {
 //                lottie_anim.setComposition(it)
 //            }
 //            .addFailureListener {
 //                Log.e(tag, "Lottie 加载失败", it)
 //            }
-
+        lottie_anim.imageAssetsFolder = "move/images/"
+        lottie_anim.setAnimation("move/data.json")
         start.setOnClickListener{
             lottie_anim.playAnimation()
         }
@@ -33,6 +36,12 @@ class MainActivity : AppCompatActivity() {
         }
         resume.setOnClickListener {
             lottie_anim.resumeAnimation()
+        }
+
+        lottie_anim.addAnimatorUpdateListener {
+            Log.d(tag, "value: " + it.animatedValue)
+            count++
+            Log.d(tag, "value: count: $count")
         }
 
     }
